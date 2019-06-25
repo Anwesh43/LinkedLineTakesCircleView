68d8c1147a31ea2a802f9eb4063e1885129a8419
Anwesh43 <anweshthecool0@gmail.com>
2019-06-25 09:33:40 +0530

    adding LTCNode class

--- linetakecircleview/src/main/java/com/anwesh/uiprojects/linetakecircleview/LineTakesCircleView.kt
+++ linetakecircleview/src/main/java/com/anwesh/uiprojects/linetakecircleview/LineTakesCircleView.kt
@@ -129,4 +129,48 @@ class LineTakesCircleView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LTCNode(var i : Int, val state : State = State()) {
+
+        private var next : LTCNode? = null
+        private var prev : LTCNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = LTCNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLTCNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LTCNode {
+            var curr : LTCNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }

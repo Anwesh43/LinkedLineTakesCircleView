628f894c096ff93c04bac3ac00baf81262ee3f0a
Anwesh43 <anweshthecool0@gmail.com>
2019-06-25 09:02:57 +0530

    adding constants

--- linetakecircleview/src/main/java/com/anwesh/uiprojects/linetakecircleview/LineTakesCircleView.kt
+++ linetakecircleview/src/main/java/com/anwesh/uiprojects/linetakecircleview/LineTakesCircleView.kt
@@ -3,3 +3,19 @@ package com.anwesh.uiprojects.linetakecircleview
 /**
  * Created by anweshmishra on 25/06/19.
  */
+
+import android.graphics.*
+import android.view.MotionEvent
+import android.view.View
+import android.app.Activity
+import android.content.Context
+
+val nodes : Int = 5
+val lines : Int = 2
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#01579B")
+val backColor : Int = Color.parseColor("#BDBDBD")
+val deg : Float = 90f

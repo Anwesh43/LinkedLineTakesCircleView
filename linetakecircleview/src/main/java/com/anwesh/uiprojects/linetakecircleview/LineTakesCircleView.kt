56cbd52db5adac4d996fec1a2c60fcec2d28d17f
Anwesh43 <anweshthecool0@gmail.com>
2019-06-25 09:35:32 +0530

    adding LineTakeCircle class

--- linetakecircleview/src/main/java/com/anwesh/uiprojects/linetakecircleview/LineTakesCircleView.kt
+++ linetakecircleview/src/main/java/com/anwesh/uiprojects/linetakecircleview/LineTakesCircleView.kt
@@ -173,4 +173,28 @@ class LineTakesCircleView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class LineTakeCircle(var i : Int) {
+
+        private val root : LTCNode = LTCNode(0)
+        private var curr : LTCNode = root
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }

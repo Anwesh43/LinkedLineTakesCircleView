39cef2d4e412864de0692aba3f9c04ece938be6c
Anwesh43 <anweshthecool0@gmail.com>
2019-07-01 10:45:51 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedlinetakescircleview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedlinetakescircleview/MainActivity.kt
@@ -2,11 +2,13 @@ package com.anwesh.uiprojects.linkedlinetakescircleview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.linetakecircleview.LineTakesCircleView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        LineTakesCircleView.create(this)
     }
 }
